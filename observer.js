7e8cc5293859e611670f3c679ca866b65ae1ec35
Alexander Kravchuk <kravchuk018@gmail.com>
2021-01-23 19:52:56 +0200

    add Observer

---
+++ observer.js
@@ -0,0 +1,66 @@
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const ADD = 'ADD';
+
+class Subject {
+    constructor() {
+        this.observers = []
+    }
+
+    subscribe(observer) {
+        this.observers.push(observer)
+    }
+    
+    unsubscribe(observer) {
+        this.observers = this.observers.filter(obs => obs !== observer)
+    }
+
+    fire(changes) {
+        this.observers.forEach(observer => observer.update(changes));
+    }
+}
+
+
+class Observer {
+    constructor(state = 1) {
+        this.state = state;
+        this.initialState = state;
+    }
+    
+    update(action) {
+        switch (action.type) {
+            case INCREMENT:
+                this.state = ++this.state;
+                break;
+            case DECREMENT:
+                this.state = --this.state;
+                break;
+            case ADD:
+                this.state += action.payload;
+                break;
+            default:
+                this.state = this.initialState
+                break;
+        }
+    }
+}
+
+const stream$ = new Subject();
+
+const obs1 = new Observer();
+const obs2 = new Observer(15);
+
+stream$.subscribe(obs1);
+stream$.subscribe(obs2);
+
+console.log(obs1.state);
+console.log(obs2.state);
+
+stream$.fire({type: INCREMENT});
+console.log(obs1.state);
+console.log(obs2.state);
+
+stream$.fire({type: ADD, payload: 10});
+
+console.log(obs1.state);
+console.log(obs2.state);
